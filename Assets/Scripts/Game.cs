59190db0ae5d577505ef28a297b48802be7951ee
Krystian Brych <krystian.brych@gmail.com>
2018-10-11 12:28:02 +0200

    Lab 2

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Assets.Scripts
+{
+    public class Game : MonoBehaviour
+    {
+        [SerializeField] private GameObject _cube;
+        [SerializeField] private float _delay;
+        private Scheduler SchedulerInstance;
+        Action a;
+        void Awake()
+        {
+            a = () =>
+            {
+                Destroy(_cube);
+            };
+        }
+        void Start()
+        {
+            SchedulerInstance = Scheduler.Instance;
+            Debug.Log(SchedulerInstance.name);
+            SchedulerInstance.ScheduleParameter(_delay, a);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
---
+++ Assets/Scripts/Scheduler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Scheduler : MonoBehaviour
+    {
+        private static Scheduler _instance;
+        private void Awake()
+        {
+            Instance = this;
+        }
+        public static Scheduler Instance
+        { get; private set; }
+        private IEnumerator RunDelayed(float t, Action action)
+        {
+            WaitForSeconds wait = new WaitForSeconds(t);
+            yield return wait;
+            action();
+        }
+        public void ScheduleParameter(float delay, Action a)
+        {
+            Debug.Log("Delay is " + delay);
+            StartCoroutine(RunDelayed(delay, a));
+        }
+    }
+}
+
